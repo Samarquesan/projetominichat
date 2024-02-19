ad932bac08090c12679e28b829a19ba29cd2553d
Samuel Marques dos Santos <78937543+Samarquesan@users.noreply.github.com>
2024-02-19 16:57:05 -0300

    Atualizações

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,8 +7,10 @@ const wss = new WebSocketServer({ port: process.env.PORT || 8080 })
 
 wss.on("connection", (ws) =>{
    ws.on("error", console.error)
+   ws.send("Servidor quer café!!!")
    ws.on("message", (data) =>{
+      //console.log(data.toString())
       wss.clients.forEach((client) => client.send(data.toString()))
    })
-   console.log("cliente coneected")
+   console.log("cliente conected")
 })
